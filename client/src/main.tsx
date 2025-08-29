8b82bd134cbb3df7683b42f1f671dd81c8e43828
KushKK29 <kush282930@gmail.com>
2025-08-29 15:31:39 +0530

    correction goggle

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from 'sonner';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-// const id= import.meta.env.GOOGLE_CLIENT_ID;
-const id='40261907168-bfruha2ttc4t3hi6neqa76l8fus2ohe1.apps.googleusercontent.com'
-console.log("Google Client ID:", id);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "sonner";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const googleClientId =
+  "1025012127670-8pv7poj127e87cqck5bhimv9qne6n65r.apps.googleusercontent.com";
+
+if (!googleClientId) {
+  // Surface a clear error during development if the client ID is missing
+  // eslint-disable-next-line no-console
+  console.error(
+    "Missing VITE_GOOGLE_CLIENT_ID env var. Set it in your environment."
+  );
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={id}>
+    <GoogleOAuthProvider clientId={googleClientId || ""}>
       <App />
-      </GoogleOAuthProvider>
+    </GoogleOAuthProvider>
     <Toaster position="top-right" richColors closeButton />
-    
   </StrictMode>
-  
 );
