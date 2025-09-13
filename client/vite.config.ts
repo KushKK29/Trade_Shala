6f126a4d9e225a84729ed0c8e4f3ae6af08b13aa
KushKK29 <kush282930@gmail.com>
2025-09-13 19:09:37 +0530

    vite.config. fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,13 +7,13 @@ export default defineConfig({
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
   preview: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
   resolve: {
